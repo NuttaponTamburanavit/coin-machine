5053be354f0bee658c10de2f521e751a4b56e624
Nuttapon <bankjumpmon@hotmail.com>
2019-10-12 17:15:52 +0700

    bind input and submit

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -3,23 +3,44 @@ import CalculatorStyle from './style';
 
 class Calculator extends Component {
   state = {
-    calc: "Input your cash."
+    inputCash: "",
+    returnCash: "Result."
   }
+
+  inputCashChange = (event) => {
+    // console.log(event.target.value)
+    let inputCash = event.target.value;
+    this.setState({ inputCash })
+  }
+
+  handleSubmit = () => {
+    const { inputCash } = this.state;
+    console.log({ inputCash })
+  }
+
   render() {
-    const { calc } = this.state;
+    const { inputCash, returnCash } = this.state;
 
     return (
       <CalculatorStyle>
         <div className="form-layout">
           <h2>COIN EXCHANGE</h2>
-          <input className="form-control"/>
-          <button className="exchange-btn">Exchange</button>
+      
+          <input className="form-control" 
+            value={inputCash} 
+            onChange={this.inputCashChange} 
+            placeholder="Your cash" 
+          />
+
+          <button className="exchange-btn" onClick={this.handleSubmit}>
+            Exchange
+          </button>
         </div>
         
         <hr/>
 
         <div className="result-container">
-          {calc}
+          {returnCash}
         </div>
       </CalculatorStyle>
     )
